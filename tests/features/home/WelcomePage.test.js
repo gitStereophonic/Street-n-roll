76cb56f762caeaf7bf64468d5cddc0ccf46da1ab
Sergey Chinkov @Work <sergey.chinkov@yandex.ru>
2018-03-06 17:30:06 +0300

    [UPD] Поправлены и дополнены некоторые тесты

--- tests/features/home/WelcomePage.test.js
+++ tests/features/home/WelcomePage.test.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import sinon from 'sinon';
-import ConnectedDefaultPage, { WelcomePage } from 'src/features/home/WelcomePage';
+import { WelcomePage } from 'src/features/home/WelcomePage';
 
-describe('features/home/DefaultPage', () => {
-  it('redux connect works', () => {
-    const pageProps = {
-      home: {},
-      actions: {},
-    };
-    const store = createStore(state => state, pageProps);
-
-    const wrapper = mount(
-      <Provider store={store}>
-        <ConnectedDefaultPage />
-      </Provider>
-    );
-
-    expect(wrapper.find('.home-welcome-page').length).to.equal(1);
-  });
-
+describe('features/home/WelcomePage', () => {
   it('should render node with correct class name', () => {
     const pageProps = {
       home: {},
--- tests/features/interview/FirstPage.test.js
+++ tests/features/interview/FirstPage.test.js
@@ -3,18 +3,30 @@ import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import { FirstPage } from 'src/features/interview/FirstPage';
 
+const props = {
+  interview: {
+    allFields: {
+      city: 'Moscow',
+      age: '18',
+      gender: 'female',
+      eduChosen: 'High-incomplete',
+    },
+  },
+  actions: {},
+};
+
 describe('interview/FirstPage', () => {
   it('renders node with correct class name', () => {
-    const props = {
-      interview: {},
-      actions: {},
-    };
-    const renderedComponent = shallow(
-      <FirstPage {...props} />
-    );
+    const renderedComponent = shallow(React.createElement(FirstPage, { ...props }));
 
-    expect(
-      renderedComponent.find('.interview-first-page').getElement()
-    ).to.exist;
+    expect(renderedComponent.find('.interview-first-page').getElement()).to.exist;
+  });
+
+  it('renders node with correct buttons class name', () => {
+    const renderedComponent = shallow(React.createElement(FirstPage, { ...props }));
+
+    expect(renderedComponent.find('.btn-disable')).to.exist;
+    expect(renderedComponent.find('.next-btn')).to.exist;
+    expect(renderedComponent.find('.prev-btn')).to.exist;
   });
 });
