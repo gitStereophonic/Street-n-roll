d996b188b67ebe3d2594cddfa51cc3b8400c5047
Sergey Chinkov @Work <sergey.chinkov@yandex.ru>
2018-03-06 19:00:02 +0300

    [FIX] Тест кнопки начала опроса

--- tests/features/interview/redux/prevPage.test.js
+++ tests/features/interview/redux/prevPage.test.js
@@ -1,13 +1,8 @@
 import { expect } from 'chai';
 
-import {
-  INTERVIEW_PREV_PAGE,
-} from 'src/features/interview/redux/constants';
+import { INTERVIEW_PREV_PAGE } from 'src/features/interview/redux/constants';
 
-import {
-  prevPage,
-  reducer,
-} from 'src/features/interview/redux/prevPage';
+import { prevPage, reducer } from 'src/features/interview/redux/prevPage';
 
 describe('interview/redux/prevPage', () => {
   it('returns correct action by prevPage', () => {
@@ -15,12 +10,18 @@ describe('interview/redux/prevPage', () => {
   });
 
   it('handles action type INTERVIEW_PREV_PAGE correctly', () => {
-    const prevState = {};
-    const state = reducer(
-      prevState,
-      { type: INTERVIEW_PREV_PAGE }
-    );
-    expect(state).to.not.equal(prevState); // should be immutable
-    expect(state).to.deep.equal(prevState); // TODO: replace this line with real case.
+    const prevState = {
+      interviewStages: [0, 33, 435, 36],
+      currentIndex: 2,
+      pageContent: 2,
+    };
+    const expectedState = {
+      interviewStages: [0, 33, 435, 36],
+      currentIndex: 1,
+      pageContent: 33,
+    };
+    const state = reducer(prevState, { type: INTERVIEW_PREV_PAGE });
+    expect(state).to.not.equal(prevState);
+    expect(state).to.deep.equal(expectedState);
   });
 });
