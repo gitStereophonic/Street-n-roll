483976148d5749e44f0543a8d4aede5414481115
Sergey Chinkov @Work <sergey.chinkov@yandex.ru>
2018-03-06 17:28:56 +0300

    [UPD] Убрано лишнее

--- src/features/home/redux/initialState.js
+++ src/features/home/redux/initialState.js
@@ -6,8 +6,6 @@
 
 // NOTE: initialState constant is necessary so that Rekit could auto add initial state when creating async actions.
 const initialState = {
-  count: 0,
-  redditReactjsList: [],
 };
 
 export default initialState;
