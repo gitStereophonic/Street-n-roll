c8c8658b391354a22624e69562905525ca04811d
Sergey Chinkov @Work <sergey.chinkov@yandex.ru>
2018-03-07 14:24:11 +0300

    [UPD] Следующая страница реализована простейшем виде

--- src/features/interview/redux/nextPage.js
+++ src/features/interview/redux/nextPage.js
@@ -2,9 +2,7 @@
 // putting related actions and reducers in one file. See more at:
 // https://medium.com/@nate_wang/a-new-approach-for-managing-redux-actions-91c26ce8b5da
 
-import {
-  INTERVIEW_NEXT_PAGE,
-} from './constants';
+import { INTERVIEW_NEXT_PAGE } from './constants';
 
 export function nextPage() {
   return {
@@ -13,10 +11,14 @@ export function nextPage() {
 }
 
 export function reducer(state, action) {
+  const curInd = state.currentIndex + 1 < state.interviewStages.count ? state.currentIndex + 1 : state.currentIndex;
+
   switch (action.type) {
     case INTERVIEW_NEXT_PAGE:
       return {
         ...state,
+        currentIndex: curInd,
+        pageContent: state.interviewStages[state.currentIndex + 1],
       };
 
     default:
