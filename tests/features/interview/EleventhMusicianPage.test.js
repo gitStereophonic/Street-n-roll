99a894698391a5f065c0330d20637177d577da72
Sergey <sergey.chinkov@yandex.ru>
2018-03-18 17:11:38 +0300

    [UPD] Натриевые тесты дополнены

--- tests/features/interview/EleventhMusicianPage.test.js
+++ tests/features/interview/EleventhMusicianPage.test.js
@@ -8,36 +8,32 @@ describe('interview/EleventhMusicianPage', () => {
     const props = {
       interview: {
         eleventhFields: { meetings: '', meetingsExact: '' },
-        checkPoints: [
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          { meetings: '', meetingsExact: '' },
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-          {},
-        ],
-        currentIndex: 11,
+        checkPoints: [{}, { meetings: '', meetingsExact: '' }],
+        currentIndex: 1,
       },
       actions: {},
     };
     const renderedComponent = shallow(React.createElement(EleventhMusicianPage, { ...props }));
 
     expect(renderedComponent.find('.interview-eleventh-musician-page').getElement()).to.exist;
+  });
+
+  it('renders node with correct value', () => {
+    const props = {
+      interview: {
+        eleventhFields: { meetings: '', meetingsExact: '' },
+        checkPoints: [{}, { meetings: 'nope', meetingsExact: 'Не знаю' }],
+        currentIndex: 1,
+      },
+      actions: {},
+    };
+    const renderedComponent = shallow(React.createElement(EleventhMusicianPage, { ...props }));
+
+    console.log(renderedComponent.find('input'));
+
+    expect(renderedComponent.find('input')).to.have.length(3);
+    expect(renderedComponent.find('input').get(0).props.defaultChecked).to.equal(false);
+    expect(renderedComponent.find('input').get(1).props.defaultChecked).to.equal(false);
+    expect(renderedComponent.find('input').get(2).props.defaultChecked).to.equal(true);
   });
 });
