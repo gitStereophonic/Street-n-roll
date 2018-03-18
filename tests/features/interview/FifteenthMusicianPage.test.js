2a8356cbc76f40fad0acd8e2e0494fed4431b2bf
Sergey <sergey.chinkov@yandex.ru>
2018-03-18 20:21:45 +0300

    [FIX] Фосфорные тесты поправлены и дополнены

--- tests/features/interview/FifteenthMusicianPage.test.js
+++ tests/features/interview/FifteenthMusicianPage.test.js
@@ -6,15 +6,55 @@ import { FifteenthMusicianPage } from 'src/features/interview/FifteenthMusicianP
 describe('interview/FifteenthMusicianPage', () => {
   it('renders node with correct class name', () => {
     const props = {
-      interview: {},
+      interview: {
+        fifteenthFields: { howcome: '', howleave: '', firstmoney: '', talk: '' },
+        checkPoints: [{}, { howcome: '', howleave: '', firstmoney: '', talk: '' }],
+        currentIndex: 1,
+      },
       actions: {},
     };
-    const renderedComponent = shallow(
-      <FifteenthMusicianPage {...props} />
-    );
+    const renderedComponent = shallow(React.createElement(FifteenthMusicianPage, { ...props }));
 
-    expect(
-      renderedComponent.find('.interview-fifteenth-musician-page').getElement()
-    ).to.exist;
+    expect(renderedComponent.find('.interview-fifteenth-musician-page').getElement()).to.exist;
+  });
+
+  it('renders node with correct input fields values', () => {
+    const props = {
+      interview: {
+        fifteenthFields: { howcome: '', howleave: '', firstmoney: '', talk: '' },
+        checkPoints: [{}, { howcome: 'how to come', howleave: 'how to leave', firstmoney: 'first money', talk: 'speak to me' }],
+        currentIndex: 1,
+      },
+      actions: {},
+    };
+    const expectedElementHowCome = React.createElement('input', {
+      defaultValue: 'how to come',
+      id: 'howcome',
+    });
+    const expectedElementHowLeave = React.createElement('input', {
+      defaultValue: 'how to leave',
+      id: 'howleave',
+    });
+    const expectedElementFirstMoney = React.createElement('input', {
+      defaultValue: 'first money',
+      id: 'firstmoney',
+    });
+    const expectedElementTalk = React.createElement('input', {
+      defaultValue: 'speak to me',
+      id: 'talk',
+    });
+    const renderedComponent = shallow(React.createElement(FifteenthMusicianPage, { ...props }));
+    expect(renderedComponent.find('#howcome').getElement().props.defaultValue).to.equal(
+      expectedElementHowCome.props.defaultValue
+    );
+    expect(renderedComponent.find('#howleave').getElement().props.defaultValue).to.equal(
+      expectedElementHowLeave.props.defaultValue
+    );
+    expect(renderedComponent.find('#firstmoney').getElement().props.defaultValue).to.equal(
+      expectedElementFirstMoney.props.defaultValue
+    );
+    expect(renderedComponent.find('#talk').getElement().props.defaultValue).to.equal(
+      expectedElementTalk.props.defaultValue
+    );
   });
 });
