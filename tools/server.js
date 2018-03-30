a272569d05b779a14708e805c46a027ab7a903ae
Sergey <sergey.chinkov@yandex.ru>
2018-03-30 13:22:08 +0300

    [UPD] Обновил бэкенд локального сервера

--- tools/server.js
+++ tools/server.js
@@ -43,6 +43,82 @@ const args = parser.parseArgs();
 const srcPath = path.join(__dirname, '../src');
 const manifestPath = path.join(__dirname, '../.tmp/dev-vendors-manifest.json');
 
+const aS = {
+  id:         { type: Sequelize.INTEGER, primaryKey: true },
+  city:       { type: Sequelize.STRING },
+  age:        { type: Sequelize.STRING },
+  gender:     { type: Sequelize.STRING },
+  edu:        { type: Sequelize.STRING },
+  eduOther:   { type: Sequelize.STRING },
+  job:        { type: Sequelize.STRING },
+  everPlayed: { type: Sequelize.BOOLEAN },
+  thanks:     { type: Sequelize.TEXT },
+  help:       { type: Sequelize.TEXT }
+};
+
+const aL = {
+  id:         { type: Sequelize.INTEGER, primaryKey: true },
+  interest:   { type: Sequelize.INTEGER },
+  who:        { type: Sequelize.STRING },
+  money:      { type: Sequelize.STRING },
+  songs:      { type: Sequelize.TEXT },
+  sign:       { type: Sequelize.TEXT },
+  traditions: { type: Sequelize.TEXT },
+  experience: { type: Sequelize.TEXT }
+};
+
+const aM = {
+  id:             { type: Sequelize.INTEGER, primaryKey: true },
+  hobbie:         { type: Sequelize.STRING },
+  hobbieOther:    { type: Sequelize.STRING },
+  rather:         { type: Sequelize.BOOLEAN },
+  ratherExact:    { type: Sequelize.STRING },
+  why:            { type: Sequelize.STRING },
+  community:      { type: Sequelize.BOOLEAN },
+  communityExact: { type: Sequelize.STRING },
+  official:       { type: Sequelize.STRING },
+  officialOther:  { type: Sequelize.STRING },
+  wocom:          { type: Sequelize.STRING },
+  howjoin:        { type: Sequelize.TEXT },
+  cookies:        { type: Sequelize.TEXT },
+  meetings:       { type: Sequelize.BOOLEAN },
+  meetingsExact:  { type: Sequelize.STRING },
+  reasons:        { type: Sequelize.TEXT },
+  where:          { type: Sequelize.TEXT },
+  whywhere:       { type: Sequelize.TEXT },
+  meetingtime:    { type: Sequelize.STRING },
+  place:          { type: Sequelize.TEXT },
+  descplace:      { type: Sequelize.TEXT },
+  time:           { type: Sequelize.TEXT },
+  whatplay:       { type: Sequelize.TEXT },
+  whythisplay:    { type: Sequelize.TEXT },
+  placeplay:      { type: Sequelize.STRING },
+  howcome:        { type: Sequelize.TEXT },
+  howleave:       { type: Sequelize.TEXT },
+  firstmoney:     { type: Sequelize.STRING },
+  talk:           { type: Sequelize.TEXT },
+  jargon:         { type: Sequelize.TEXT },
+  specsigns:      { type: Sequelize.TEXT },
+  idmarks:        { type: Sequelize.TEXT },
+  forwhat:        { type: Sequelize.STRING },
+  forwhatOther:   { type: Sequelize.STRING },
+  celebrations:   { type: Sequelize.STRING },
+  howceleb:       { type: Sequelize.TEXT },
+  competition:    { type: Sequelize.STRING },
+  relations:      { type: Sequelize.TEXT },
+  whobest:        { type: Sequelize.TEXT },
+  events:         { type: Sequelize.TEXT },
+  reactions:      { type: Sequelize.TEXT },
+  story:          { type: Sequelize.TEXT },
+  identity:       { type: Sequelize.TEXT }
+};
+
+const aSettings = {
+  createdAt: false,
+  updatedAt: false,
+  freezeTableName: true
+}
+
 
 // Start an express server for development using webpack dev-middleware and hot-middleware
 function startDevServer() {
@@ -84,9 +160,6 @@ function startDevServer() {
     const dataBase = req.body.dataBase;
     const aStart = req.body.answersStart;
     const aTable = req.body.answersTable;
-    console.log(dataBase);
-    console.log(aStart);
-    console.log(aTable);
 
     const sequelize = new Sequelize('StreetnrollDB', 'sergey.chinkov', 'RRica29081BhA5', {
       host: 'localhost',
@@ -108,87 +181,11 @@ function startDevServer() {
       .then(() => {
         console.log('Connection established');
 
-        var answersStart = sequelize.define('answersStart', {
-          id:         { type: Sequelize.INTEGER, primaryKey: true },
-          city:       { type: Sequelize.STRING },
-          age:        { type: Sequelize.STRING },
-          gender:     { type: Sequelize.STRING },
-          edu:        { type: Sequelize.STRING },
-          eduOther:   { type: Sequelize.STRING },
-          job:        { type: Sequelize.STRING },
-          everPlayed: { type: Sequelize.BOOLEAN },
-          thanks:     { type: Sequelize.TEXT },
-          help:       { type: Sequelize.TEXT }
-        }, {
-          createdAt: false,
-          updatedAt: false,
-          freezeTableName: true
-        });
+        const answersStart = sequelize.define('answersStart', aS, aSettings);
 
-        var answersListener = sequelize.define(dataBase, {
-          id:         { type: Sequelize.INTEGER, primaryKey: true },
-          interest:   { type: Sequelize.INTEGER },
-          who:        { type: Sequelize.STRING },
-          money:      { type: Sequelize.STRING },
-          songs:      { type: Sequelize.TEXT },
-          sign:       { type: Sequelize.TEXT },
-          traditions: { type: Sequelize.TEXT },
-          experience: { type: Sequelize.TEXT }
-        }, {
-          createdAt: false,
-          updatedAt: false,
-          freezeTableName: true
-        });
+        const answersListener = sequelize.define(dataBase, aL, aSettings);
 
-        var answersMusician = sequelize.define(dataBase, {
-          id:             { type: Sequelize.INTEGER, primaryKey: true },
-          hobbie:         { type: Sequelize.STRING },
-          hobbieOther:    { type: Sequelize.STRING },
-          rather:         { type: Sequelize.BOOLEAN },
-          ratherExact:    { type: Sequelize.STRING },
-          why:            { type: Sequelize.STRING },
-          community:      { type: Sequelize.BOOLEAN },
-          communityExact: { type: Sequelize.STRING },
-          official:       { type: Sequelize.STRING },
-          officialOther:  { type: Sequelize.STRING },
-          wocom:          { type: Sequelize.STRING },
-          howjoin:        { type: Sequelize.TEXT },
-          cookies:        { type: Sequelize.TEXT },
-          meetings:       { type: Sequelize.BOOLEAN },
-          meetingsExact:  { type: Sequelize.STRING },
-          reasons:        { type: Sequelize.TEXT },
-          where:          { type: Sequelize.TEXT },
-          whywhere:       { type: Sequelize.TEXT },
-          meetingtime:    { type: Sequelize.STRING },
-          place:          { type: Sequelize.TEXT },
-          descplace:      { type: Sequelize.TEXT },
-          time:           { type: Sequelize.TEXT },
-          whatplay:       { type: Sequelize.TEXT },
-          whythisplay:    { type: Sequelize.TEXT },
-          placeplay:      { type: Sequelize.STRING },
-          howcome:        { type: Sequelize.TEXT },
-          howleave:       { type: Sequelize.TEXT },
-          firstmoney:     { type: Sequelize.STRING },
-          talk:           { type: Sequelize.TEXT },
-          jargon:         { type: Sequelize.TEXT },
-          specsigns:      { type: Sequelize.TEXT },
-          idmarks:        { type: Sequelize.TEXT },
-          forwhat:        { type: Sequelize.STRING },
-          forwhatOther:   { type: Sequelize.STRING },
-          celebrations:   { type: Sequelize.STRING },
-          howceleb:       { type: Sequelize.TEXT },
-          competition:    { type: Sequelize.STRING },
-          relations:      { type: Sequelize.TEXT },
-          whobest:        { type: Sequelize.TEXT },
-          events:         { type: Sequelize.TEXT },
-          reactions:      { type: Sequelize.TEXT },
-          story:          { type: Sequelize.TEXT },
-          identity:       { type: Sequelize.TEXT }
-        }, {
-          createdAt: false,
-          updatedAt: false,
-          freezeTableName: true
-        });
+        const answersMusician = sequelize.define(dataBase, aM, aSettings);
 
         answersStart.sync().then(function () {
           answersStart.findAll().then(function(rows) {
@@ -305,6 +302,138 @@ function startBuildServer() {
   const root = path.join(__dirname, '../build');
   app.use(express.static(root));
   app.use(fallback('index.html', { root }));
+
+  app.use(express.static(path.join(__dirname, '../')));
+
+  app.use(bodyParser.json());
+
+  app.post('/send', (req, res) => {
+    const dataBase = req.body.dataBase;
+    const aStart = req.body.answersStart;
+    const aTable = req.body.answersTable;
+
+    console.log(dataBase);
+    console.log(aStart);
+    console.log(aTable);
+
+    const sequelize = new Sequelize('StreetnrollDB', 'sergey.chinkov@yandex.ru', 'RRica29081BhA5', {
+      host: 'localhost',
+      dialect: 'sqlite',
+
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+
+      storage: './StreetnrollDB.db',
+
+      operatorsAliases: false
+    });
+
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log('Connection established');
+
+        const answersStart = sequelize.define('answersStart', aS, aSettings);
+
+        const answersListener = sequelize.define(dataBase, aL, aSettings);
+
+        const answersMusician = sequelize.define(dataBase, aM, aSettings);
+
+        answersStart.sync().then(function () {
+          answersStart.findAll().then(function(rows) {
+            answersStart.create({
+              id:         rows.length,
+              city:       aStart.city,
+              age:        aStart.age,
+              gender:     aStart.gender,
+              edu:        aStart.edu,
+              eduOther:   aStart.eduOther,
+              job:        aStart.job,
+              everPlayed: aStart.everPlayed,
+              thanks:     aStart.thanks,
+              help:       aStart.help
+            }).then(function () {
+              console.log('start writen');
+              if (dataBase === 'answersListener') {
+                answersListener.sync().then(function() {
+                  answersListener.create({
+                    id:         rows.length,
+                    interest:   aTable.interest,
+                    who:        aTable.who,
+                    money:      aTable.money,
+                    songs:      aTable.songs,
+                    sign:       aTable.sign,
+                    traditions: aTable.traditions,
+                    experience: aTable.experience
+                  }).then(function() {
+                    res.sendStatus(200);
+                  });
+                });
+              } else if (dataBase === 'answersMusician') {
+                answersMusician.sync().then(function() {
+                  answersMusician.create({
+                    id:             rows.length,
+                    hobbie:         aTable.hobbie,
+                    hobbieOther:    aTable.hobbieOther,
+                    rather:         aTable.rather,
+                    ratherExact:    aTable.ratherExact,
+                    why:            aTable.why,
+                    community:      aTable.community,
+                    communityExact: aTable.communityExact,
+                    official:       aTable.official,
+                    officialOther:  aTable.officialOther,
+                    wocom:          aTable.wocom,
+                    howjoin:        aTable.howjoin,
+                    cookies:        aTable.cookies,
+                    meetings:       aTable.meetings,
+                    meetingsExact:  aTable.meetingsExact,
+                    reasons:        aTable.reasons,
+                    where:          aTable.where,
+                    whywhere:       aTable.whywhere,
+                    meetingtime:    aTable.meetingtime,
+                    place:          aTable.place,
+                    descplace:      aTable.descplace,
+                    time:           aTable.time,
+                    whatplay:       aTable.whatplay,
+                    whythisplay:    aTable.whythisplay,
+                    placeplay:      aTable.placeplay,
+                    howcome:        aTable.howcome,
+                    howleave:       aTable.howleave,
+                    firstmoney:     aTable.firstmoney,
+                    talk:           aTable.talk,
+                    jargon:         aTable.jargon,
+                    specsigns:      aTable.specsigns,
+                    idmarks:        aTable.idmarks,
+                    forwhat:        aTable.forwhat,
+                    forwhatOther:   aTable.forwhatOther,
+                    celebrations:   aTable.celebrations,
+                    howceleb:       aTable.howceleb,
+                    competition:    aTable.competition,
+                    relations:      aTable.relations,
+                    whobest:        aTable.whobest,
+                    events:         aTable.events,
+                    reactions:      aTable.reactions,
+                    story:          aTable.story,
+                    identity:       aTable.identity
+                  }).then(function() {
+                    res.sendStatus(200);
+                  });
+                });
+              } else {
+                res.sendStatus(400);
+              }
+            });
+          });
+        });
+      })
+      .catch(err => {
+        console.error('Connection Error: ', err);
+        res.sendStatus(502);
+      });
+  });
 
   // Other files should not happen, respond 404
   app.get('*', (req, res) => {
