8defa34f2f866f452bc8a387c74a56344b796d1e
Sergey <sergey.chinkov@yandex.ru>
2018-07-14 01:01:44 +0300

    [ADD] Добавлен объект для отображения страниц слева страницы

---
+++ src/features/the-data-pick-up/Pages.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from './redux/actions';
+
+export class Pages extends Component {
+  static propTypes = {
+    theDataPickUp: PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const { getData } = this.props.actions;
+    const { components } = this.props.theDataPickUp;
+    const pagesToRender = [];
+    return React.createElement(
+      'div',
+      { className: 'the-data-pick-up-pages' },
+      React.createElement(
+        'p',
+        {
+          className: 'statPanel',
+          onClick: () => {
+            this.props.theDataPickUp.currentComponent = components.users;
+            getData();
+          },
+        },
+        'Respondents'
+      ),
+      pagesToRender
+    );
+  }
+}
+
+/* istanbul ignore next */
+function mapStateToProps(state) {
+  return {
+    theDataPickUp: state.theDataPickUp,
+  };
+}
+
+/* istanbul ignore next */
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({ ...actions }, dispatch),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Pages);
